24d03a0cbcd6c34b89f2330c9b5d86fa5753042f
maxpavlenko <32528661+maxpavlenko@users.noreply.github.com>
2019-09-10 22:54:50 -0500

    edits

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 let myAge = 31;
 //This is my age
+
 let earlyYears = 2;
 //This are my early years
+
 earlyYears *= 10.5;
 let laterYears = myAge - 2;
 //Later age calculation
+
 laterYears = laterYears * 4;
 //Lattet years calculation
+
 console.log(earlyYears);
 console.log(laterYears);
 //Show my age
+
 myAgeInDogYears = earlyYears + laterYears;
 //Ages together
+
 myName = 'Maksym'.toLowerCase();
 console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
