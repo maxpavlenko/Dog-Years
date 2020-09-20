2ca9a56b122641f62f4ebaf809a99861b6fb801c
maxpavlenko <32528661+maxpavlenko@users.noreply.github.com>
2020-09-20 10:48:00 -0500

    new update

--- app.js
+++ app.js
@@ -20,5 +20,3 @@ myAgeInDogYears = earlyYears + laterYears;
 
 myName = 'Maksym'.toLowerCase();
 console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
-
-//edit in VS
